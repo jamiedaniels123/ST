24bf5253be67d632db4d66fd16a814cb453f5fc2
Paul Bishop <paulweb2@gmail.com>
2012-01-27 21:52:26 +0000

    svb delete part

--- admin.py
+++ admin.py
@@ -1,40 +1,102 @@
-import os 
+# -*- coding: utf-8 -*-
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
 import cgi
 import helperfunctions
-from google.appengine.ext import db
+import uuid
+import Cookie
+from types import *
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from dbmodel import User, Table
+from google.appengine.ext import db
+from dbmodel import User, Table, Team, Result
+from datetime import datetime
  
 class MainPage(webapp.RequestHandler):
     def get(self):
         u = User.all()
         url = '../'
         url_linktext = 'Home'
+        disable = 1
         template_values = {
-            'userdata': u,
-            'url': url,
-	    'url_linktext': url_linktext
+            'disable': disable
             }
         path = os.path.join(os.path.dirname(__file__), 'admin/admin.html')
         self.response.out.write(template.render(path, template_values))
 
-class DeleteUser(webapp.RequestHandler):
+class GetSpecialReport(webapp.RequestHandler):
     def post(self):
-        userk = db.Key.from_path('User', int(self.request.get('user_to_delete')))
-        userd = db.get(userk)
-        tbls = Table.all()
-        tbls.filter('user = ', userk)
-        for tbl in tbls:
-            helperfunctions.deletetable(tbl.key())
-        userd.delete()
-        self.redirect('/admin/')        
+        disable = 0
+        #get tables that match
+        tot_ts = Table.gql("")
+        #add the beginning count
+        total_tables = tot_ts.count()
+        ts = Table.gql("WHERE name IN('SVB', :1)", u'SV Böblingen D1')
+        #add the number affected with SVB and SV B
+        total_bad = ts.count()
+        deletecounter = 0
+        for t in ts:
+            teams = Team.gql("WHERE table = :1", t)
+            if teams.count() == 0:
+                deletecounter += 1
+        #add the number of would be deleted tables        
+        delete_tables = deletecounter        
+        post_ts = Table.gql("WHERE name IN('SVB', :1)", u'SV Böblingen D1')
+        #add the number remainig after the delete operation
+        remaining_tables = post_ts.count()
+        template_values = {
+            'total_tables': total_tables,
+            'total_bad': total_bad,
+            'delete_tables': delete_tables,
+            'remaining_tables': remaining_tables,
+            'disable': disable
+            }
+        path = os.path.join(os.path.dirname(__file__), 'admin/admin.html')
+        self.response.out.write(template.render(path, template_values))
+
+class DoSpecialDeleteOp(webapp.RequestHandler):
+    def post(self):
+        disable = 1
+        #list to hold IDs of matched tables without teams
+        no_team_tables = []
+        #get tables that match
+        tot_ts = Table.gql("")
+        #add the beginning count
+        total_tables = tot_ts.count()
+        ts = Table.gql("WHERE name IN('SVB', :1)", u'SV Böblingen D1')
+        #add the number affected with SVB and SV B
+        total_bad = ts.count()
+        deletecounter = 0
+        for t in ts:
+            teams = Team.gql("WHERE table = :1", t)
+            if teams.count() == 0:
+                #do the delete thing
+                helperfunctions.deletetable(t.key())         
+                deletecounter += 1
+        #add the number of deleted tables        
+        delete_tables = deletecounter        
+        post_ts = Table.gql("WHERE name IN('SVB', :1)", u'SV Böblingen D1')
+        #add the number remainig after the delete operation
+        remaining_tables = post_ts.count()
+        template_values = {
+            'total_tables': total_tables,
+            'total_bad': total_bad,
+            'delete_tables': delete_tables,
+            'remaining_tables': remaining_tables,
+            'disable': disable
+            }
+        path = os.path.join(os.path.dirname(__file__), 'admin/admin.html')
+        self.response.out.write(template.render(path, template_values))        
         
 application = webapp.WSGIApplication(
                                      [('/admin/', MainPage),
-                                      ('/admin/deleteuser', DeleteUser)],
+                                      ('/admin/getspecialreport', GetSpecialReport),
+                                      ('/admin/dospecialdeleteop', DoSpecialDeleteOp)],
                                      debug=True)
  
 def main():
